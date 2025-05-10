ae747981fc11e315e3aa00ff6da0f76af89d74cf
shahadat <nayemshahadat581@gmail.com>
2025-05-10 23:23:36 +0600

    logo changed

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-const Resume=()=>{
-    return(
-        <div className="mx-auto mt-20 w-[95%] p-2 bg-orange-300 rounded-md min-h-[20vh] md:w-[50%]">
-              <h1 className="text-white text-3xl text-center
-              font-medium mt-2 ">My Resume</h1>
-           <a
-           className="text-white font-medium text-center block mt-3 text-[18px]
-           " href="https://docs.google.com/document/d/1e5Iz6iEULBFr4rXzLSItfXgNXdraFmKnfKomfwJ9z50/edit?usp=sharing">
-           Click Here To See Resume Or Download From There</a>
-        </div>
-    )
-}
-export default Resume
+const Resume = () => {
+  return (
+    <div className="mx-auto mt-20 w-[95%] max-w-2xl p-6 bg-gradient-to-r from-blue-500 to-blue-600 rounded-xl shadow-lg">
+      <h1 className="text-white text-4xl font-semibold text-center mb-4">
+        My Resume
+      </h1>
+      <a
+        href="https://docs.google.com/document/d/1e5Iz6iEULBFr4rXzLSItfXgNXdraFmKnfKomfwJ9z50/edit?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block text-center text-white text-lg font-medium underline hover:text-gray-200 transition duration-200"
+      >
+        📄 Click here to view or download my resume
+      </a>
+    </div>
+  );
+};
+
+export default Resume;
